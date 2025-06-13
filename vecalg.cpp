d95c1960c4a917ff8e0e392a5e7030a6efc38956
feportoa <feporto.a@gmail.com>
2025-06-13 13:24:54 -0300

    Added comments to header file and added function setColor - git diff --staged vecalg.h! to be changed

--- vecalg.cpp
+++ vecalg.cpp
@@ -1,22 +1,35 @@
 #include "vecalg.h"
 
-v2 v2_add(v2 a, v2 b) {
-    v2 res = {a.x + b.x, a.y + b.y};
+// 2D Vectors
+vec2 v2_add(vec2 a, vec2 b) {
+    vec2 res = {a.x + b.x, a.y + b.y};
     return res;
 }
 
-v2 v2_subtract(v2 a, v2 b) {
-    v2 res = {a.x - b.x, a.y - b.y};
+vec2 v2_subtract(vec2 a, vec2 b) {
+    vec2 res = {a.x - b.x, a.y - b.y};
     return res;
 }
 
-v2 v2_scalar_multiplication(float a, v2 b) {
-    v2 res = {a * b.x, a * b.y};
+vec2 v2_scalar_multiplication(float a, vec2 b) {
+    vec2 res = {a * b.x, a * b.y};
     return res;
 }
 
-float v2_dot(v2 a, v2 b) {
-    v2 multplied = {a.x * b.x, a.y * b.y};
+float v2_dot(vec2 a, vec2 b) {
+    vec2 multiplied = {a.x * b.x, a.y * b.y};
     float res = multiplied.x + multiplied.y;
     return res;
 }
+
+// To-Do 3D Vectors
+
+
+// Color functions - Using vec3
+void setColor(vec3 *vec, float r, float g, float b) {
+    vec->x = r;
+    vec->y = g;
+    vec->z = b;
+    return;
+}
+
--- vecalg.h
+++ vecalg.h
@@ -12,11 +12,63 @@ typedef struct {
     float z;
 } vec3;
 
-v2 v2_add(v2 a, v2 b); // Do NOT use pointers, can overwrite original values
-v2 v2_subtract(v2 a, v2 b);
-v2 v2_scalar_multiplication(float a, v2 b);
-v2 v2_scalar_division(float a, v2 b);
-float v2_dot(v2 a, v2 b);
-float v2_cross(v2 a, v2 b);
+/**
+ * @brief Sum operation with 2 different vectors
+ *
+ * @param vec2 a First vector
+ * @param vec2 b Second vector
+ *
+ * @return vec2 Returns a vector with the results
+ */
+vec2 v2_add(vec2 a, vec2 b); 
 
+/**
+ * @brief Subtraction operation with 2 different vectors
+ *
+ * @param vec2 a First vector
+ * @param vec2 b Second vector
+ *
+ * @return vec2 Returns a vector with the results
+ */
+vec2 v2_subtract(vec2 a, vec2 b);
+
+/**
+ * @brief Multiplication between a scalar and a vector
+ *
+ * @param float a Scalar value
+ * @param vec2 b Vector value
+ *
+ * @return vec2 Returns a vector with the results
+ */
+vec2 v2_scalar_multiplication(float a, vec2 b);
+
+/**
+ * @brief Division between a scalar and a vector
+ *
+ * @param float a Scalar value
+ * @param vec2 b Vector value
+ *
+ * @return vec2 Returns a vector with the results
+ */
+vec2 v2_scalar_division(float a, vec2 b);
+
+/**
+ * @brief Makes a dot product operation
+ *
+ * @param vec2 a First vector
+ * @param vec2 b Second vector
+ *
+ * @return Float with the result
+ */
+float v2_dot(vec2 a, vec2 b);
+
+/**
+ * @brief Modifies a specific vec3 color pointer 
+ *
+ * @param vec3 vec Pointer to your color vec3
+ * @param r Red color
+ * @param g Green color
+ * @param b Blue color
+ */
+void setColor(vec3 *vec, float r, float g, float b);
 #endif
